e93c628e6fee9f33346caf988d6536fa15517049
aime <aime.nobrega@gmail.com>
2021-12-11 15:26:06 -0300

    day3

--- day3.py
+++ day3.py
@@ -11,34 +11,107 @@ by the epsilon rate, both in decimal.
 
 '''
 
+import numpy as np
 txt = "day3_data.txt"
 
 with open(txt) as f:
-    report = f.readlines()
+    file = f.readlines()
 
-print(report)
 
-report_l = []
-for line in report:
-    temp = [int(i) for i in line[:-1]]
-    report_l.append(temp)
+def file_to_matrix(file):
+    matrix = []
+    for line in file:
+        temp = [int(i) for i in line[:-1]]
+        matrix.append(temp)
+    return matrix
 
-sum = [sum(i) for i in zip(*report_l)]
-print(sum)
-print(len(report_l))
 
-gamma, epsilon = '', ''
+def common_values(matrix):  # returns list
+    sums = [sum(line) for line in zip(*matrix)]
+    values = []
+    for i in sums:
+        if i > (len(matrix)/2):
+            values.append(1)
+        else:
+            values.append(0)
+    return values
 
-for number in sum:
-    if number > 500:  # most common = 1
-        gamma += '1'
-        epsilon += '0'
-    else:
-        gamma += '0'
-        epsilon += '1'
 
-gamma = int(gamma, 2)
-epsilon = int(epsilon, 2)
+def gamma_epsilon(values):
+    gamma, epsilon = '', ''
+    for i in values:
+        if i == 0:
+            gamma += '0'
+            epsilon += '1'
+        else:
+            gamma += '1'
+            epsilon += '0'
+    return int(gamma, 2), int(epsilon, 2)
+
+
+report_matrix = file_to_matrix(file)
+gamma, epsilon = gamma_epsilon(common_values(report_matrix))
 
 power = gamma * epsilon
 print(power)
+
+
+'''
+
+life support rating = oxygen generator rating * CO2 scrubber rating
+
+oxygen generator rating = find most common value (0 or 1) in the bit
+    position
+
+
+'''
+
+
+def find_oxygen(matrix):
+    for i in range(matrix.shape[1]):
+        sum_ = np.sum(matrix[:, i])
+        if sum_ >= matrix.shape[0]/2:
+            common = 1
+        else:
+            common = 0
+        index_to_delete = []
+        for j in range(matrix.shape[0]):
+            if matrix[j][i] != common:
+                index_to_delete.append(j)
+        matrix = np.delete(matrix, index_to_delete, axis=0)
+        if matrix.shape[0] == 1:
+            return list_to_decimal(matrix[0])
+    return list_to_decimal(matrix[0])
+
+def find_CO2(matrix):
+    for i in range(matrix.shape[1]):
+        sum_ = np.sum(matrix[:, i])
+        if sum_ >= matrix.shape[0]/2:
+            least_common = 0
+        else:
+            least_common = 1
+        index_to_delete = []
+        for j in range(matrix.shape[0]):
+            if matrix[j][i] != least_common:
+                # print(f'{matrix[j][i]} diferente de {least_common}: {matrix[j]}')
+                index_to_delete.append(j)
+        matrix = np.delete(matrix, index_to_delete, axis=0)
+        if matrix.shape[0] == 1:
+            return list_to_decimal(matrix[0])
+    return list_to_decimal(matrix[0])
+
+
+def list_to_decimal(list):
+    decimal = ''
+    for i in list:
+        decimal += str(i)
+    return int(decimal, 2)
+
+
+report_matrix = file_to_matrix(file)
+report_matrix = np.array(report_matrix, dtype=int)
+print(report_matrix)
+
+oxygen = find_oxygen(report_matrix)
+CO2 = find_CO2(report_matrix)
+print(oxygen * CO2)
